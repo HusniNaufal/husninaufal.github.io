56b2ac370034ee02206d334d21acd1cb5da225b0
HusniNaufal <hsnuniversity23@gmail.com>
2025-03-31 06:52:32 +0700

    Update script.js

--- tokped/script.js
+++ tokped/script.js
@@ -1,1 +1,9 @@
 alert ("web ini hanya sekedar tampilan, tidak berfungsi");
+function isMobileDevice() {
+    return /mobile|android|iphone|ipad|ipod/i.test(navigator.userAgent);
+}
+
+if (isMobileDevice()) {
+    alert("Akses ditolak! Silakan gunakan PC atau Laptop.");
+    history.back(); // Kembali ke halaman sebelumnya
+}
